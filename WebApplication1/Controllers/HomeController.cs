679d1ed42f83cf3845a2540b41a383de6ce04fa4
vadymSarvyk <kharlamenkovadym@gmail.com>
2022-09-06 18:45:42 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Phone> phones;
+        List<Company> companies;
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            Company apple = new Company { Id = 1, Name = "Apple", Country = "США" };
+            Company microsoft = new Company { Id = 2, Name = "Samsung", Country = "Республика Корея" };
+            Company google = new Company { Id = 3, Name = "Google", Country = "США" };
+            companies = new List<Company> { apple, microsoft, google };
+            phones = new List<Phone>
+            {
+                new Phone { Id=1, Manufacturer= apple, Name="iPhone X", Price=56000 },
+                new Phone { Id=2, Manufacturer= apple, Name="iPhone XZ", Price=41000 },
+                new Phone { Id=3, Manufacturer= microsoft, Name="Galaxy 9", Price=9000 },
+                new Phone { Id=4, Manufacturer= microsoft, Name="Galaxy 10", Price=40000 },
+                new Phone { Id=5, Manufacturer= google, Name="Pixel 2", Price=30000 },
+                new Phone { Id=6, Manufacturer= google, Name="Pixel XL", Price=50000 }
+            };
+
+        }
+
+        public IActionResult Index(int? companyId)
+        {
+            List<CompanyModel> compModels = companies
+                .Select(c => new CompanyModel { Id = c.Id, Name = c.Name })
+                .ToList();
+            
+            compModels.Insert(0, new CompanyModel { Id = 0, Name = "Все" });
+
+            IndexViewModel ivm = new IndexViewModel { Companies = compModels, Phones = phones };
+
+            // если передан id компании, фильтруем список
+            if (companyId != null && companyId > 0)
+                ivm.Phones = phones.Where(p => p.Manufacturer.Id == companyId);
+
+            return View(ivm);
+
+          /*  return View(new List<string> { "Lumia 950", "iPhone 6S", "Samsung Galaxy s 6", "LG G 4" });*/
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Phone> Phones { get; set; }
+        public IEnumerable<CompanyModel> Companies { get; set; }
+    }
+}
+
